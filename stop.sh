712131fbdc051fa4f86c215468fd396e7e2b3605
Kobs <sergeik30@gmail.com>
2023-04-13 21:03:56 +0300

    scripts

--- stop.sh
+++ stop.sh
@@ -3,10 +3,10 @@ set -e
 set +x
 cd microservices
 
-kill -9 $(lsof -t -i:8018) || echo "WARNING: Nothing running on port 8018, or failed to kill processes"
-kill -9 $(lsof -t -i:3305) || echo "WARNING: Nothing running on port 3305, or failed to kill processes"
-kill -9 $(lsof -t -i:9970) || echo "WARNING: Nothing running on port 9970, or failed to kill processes"
-kill -9 $(lsof -t -i:8071) || echo "WARNING: Nothing running on port 8071, or failed to kill processes"
-kill -9 $(lsof -t -i:8009) || echo "WARNING: Nothing running on port 8009, or failed to kill processes"
+btc=$(pgrep sai-btc) && [ "$btc" != "" ] && kill -9 $btc && echo "BTC stopped" || echo "WARNING: BTC not running, or failed to stop"
+bft=$(pgrep sai-bft) && [ "$bft" != "" ] && kill -9 $bft && echo "BFT stopped" || echo "WARNING: BFT not running, or failed to stop"
+p2p=$(pgrep p2p_lin64) && [ "$p2p" != "" ] && kill -9 $p2p && echo "P2P stopped" || echo "WARNING: P2P not running, or failed to stop"
+p2pr=$(pgrep sai-p2p) && [ "$p2pr" != "" ] && kill -9 $p2pr && echo "P2P proxy stopped" || echo "WARNING: P2P proxy not running, or failed to stop"
+vm=$(pgrep sai-vm1) && [ "$vm" != "" ] && kill -9 $vm && echo "VM stopped" || echo "WARNING: VM not running, or failed to stop"
 
 docker-compose -f docker-compose-windows.yml down
